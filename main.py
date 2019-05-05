5fe7446758e70615bd82f703c90edbddc4a2d137
Thiago <thhiaggo1@hotmail.com>
2019-05-05 16:07:27 -0300

    versao michel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #coding= utf -8
-from pro_mem import*
+from version_Beta import*
 
 #------------------------------------------------------------#
 atual_clock = 0
@@ -19,8 +19,10 @@ for i in proce :
     line=i.split("\n")[0]
     cell = line.split(" ")
     List.append(cell)
-
+    
 for i in List:
     Var = process(int(i[0]),int(i[1]),int(i[2]),int(i[3]))
     list_proc.append(Var)
 list_proc = sorted(list_proc, key = process.get_arr)
+
+#---------------------------------------------------------------#
--- pro_mem.py
+++ pro_mem.py
@@ -4,22 +4,49 @@ class process():
         self.__t_arrival = arr
         self.__size = beg
         self.__t_run = run
-        self.__t_end = 0
+        self.__t_end = None
     
     def get_arr(self):
         return self.__t_arrival
+    
+    def get_end(self):
+        return self.__t_end
+    
+    def get_size(self):
+        return self.__size
 
     def modify(self,clock):
         self.__t_end = self.__t_run + clock
 
-
-class memory():
-    def __init__(self,size,st,ids):
+class cell_memory():
+    def __init__(self,size,ids):
         self.__size = size
-        self.__status = st
         self.__id_m = ids
+    def modify(self,size):
+        self.__size = size
+    def get_size(self):
+        return self.__size
+    def get_ids(self):
+        return self.__id_m
 
-    def get_info(self):
-        return [self.__size, self.__id_m]
+class mem_virtual():
+    def __init__(self):
+        self.__memory = []
+        self.__memory.append(cell_memory(1024,0))
+    
+    def new_hole(self):
+
+    def inp_proce(self,proce,hole,clock):
+        self.__memory.insert(hole.get_ids(),proce)#fazer um for no cod pra achar a posicao dodo buraco
+        hole.modify(hole.get_size-proce.get_size)#ver com michel se tem q returna ou funciona como ponteiro 
+        proce.modify(clock)#ver com michel se tem q returna ou funciona como ponteiro
+
+    def out_proce(self):
+
+    def checking(self)
+    def dsc_hole(self):
+
+    def dsc_pro(self):
+        
 
 #-------------------------------------------#
