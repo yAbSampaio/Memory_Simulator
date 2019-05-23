1d3f792ea2c27c216abea88f49780f3d14760658
Thiago <thhiaggo1@hotmail.com>
2019-05-23 18:36:10 -0300

    V_2305

--- graphic.py
+++ graphic.py
@@ -5,7 +5,9 @@ import matplotlib.ticker as mtick
 from matplotlib import style
 import pandas as pd
 from memory import*
+from dsutil import plotting
 
+'''
 style.use('fivethirtyeight')
 def graph(mem,clock):
     xs,ys = mem.mem_reciever(clock)
@@ -23,4 +25,40 @@ def graph(mem,clock):
     plt.title('Amount of records by Gender and State, normalized')
     # plt.legend(loc='lower right')
     plt.gcf().set_size_inches(7,4)
-    plt.show()
+    plt.show()'''
+
+def mem_reciever(self,clock):
+        mem = []
+        clock_list = []
+        for i in self.memory:
+            if isinstance(i,cell_memory):
+                for j in range(i.get_size()):
+                    mem.append('F')
+                    clock_list.append(clock)
+            elif isinstance(i,process):
+                for j in range(i.get_size()):
+                    mem.append('O')
+                    clock_list.append(clock)
+        return clock_list,mem
+
+    #style.use('fivethirtyeight')
+    
+    def graph(self,clock):
+        xs,ys = self.mem_reciever(clock)
+
+        df = pd.DataFrame({
+            'time':pd.Series(xs),
+            'process': pd.Series(ys),
+        })
+        df[['time','process']]
+
+        df.groupby(['time','process']).size().groupby(level=0).apply(
+            lambda x: 100 * x / x.sum()
+        ).unstack().plot(kind='bar',stacked=True,legend='reverse')
+
+        plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter())
+        plt.title('Amount of records by Gender and State, normalized')
+        # plt.legend(loc='lower right')
+        plt.gcf().set_size_inches(7,4)
+        plt.show()
+
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #coding= utf-8
-from graphic import*
+#from graphic import*
 from process import*
 from memory import*
 import time
@@ -9,8 +9,10 @@ t_apply = 0
 t_wait = 0 #t_inicio_clock - t_chegada
 t_fail = 0 #tentativas falhas
 t_medio = 0 
+n_wait = 0
 
 #------------------------inicialização---------------------------------#
+
 proce = open("process.txt","r")
 List = []
 list_proc = []
@@ -27,45 +29,52 @@ for i in List: #cria lista de classe processo
     Var = process(int(i[0]),int(i[1]),int(i[2]),int(i[3]))
     list_proc.append(Var)
 list_proc = sorted(list_proc, key = process.get_arr)
-choice = int(input("Digite\n1 - First Fit\n2 - Best Fit\n3 - Worst Fit\n"))
 
 #---------------------------------------------------------------#
+
 #Arruma logica de parada
-n_wait = 0
 while (Memo.get_len() != 1 or n_wait < len(list_proc)):#Enquato haver processo na lista 
-    if(n_wait < len(list_proc)):
-        while ((n_wait < len(list_proc)) and (list_proc[n_wait].get_arr() <= atual_clock)):#Fazer lista de espera
-            #Chamada da função first, best worst
-            if(choice == 1):
-                Memo.FirstFit(list_proc[n_wait],atual_clock)
+    #if(n_wait < len(list_proc)):
+    par = True
+    while (n_wait < len(list_proc) and (list_proc[n_wait].get_arr() <= atual_clock) and par):#Fazer lista de espera
+        #Chamada da função first, best worst
+        if(choice == 1):
+
+            if(Memo.FirstFit(list_proc[n_wait],atual_clock)):
+                t_wait += atual_clock-list_proc[n_wait].get_arr()
                 n_wait += 1
-            elif(choice == 2):
-                if(Memo.BestFit(list_proc[n_wait],atual_clock)):
-                    t_wait += atual_clock-list_proc[n_wait].get_arr()
-                    n_wait += 1#Logica de t wait
-                else:
-                    t_fail += 1
-            elif(choice == 3):
-                if(Memo.WorstFit(list_proc[n_wait],atual_clock)):
-                    t_wait += atual_clock-list_proc[n_wait].get_arr()
-                    n_wait += 1#Logica de t wait
-                else:
-                    t_fail += 1
+            else:
+                control = False
+
+        elif(choice == 2):
+
+            if(Memo.BestFit(list_proc[n_wait],atual_clock)):
+                t_wait += atual_clock-list_proc[n_wait].get_arr()
+                n_wait += 1#Logica de t wait
+            else:
+                control = False
+        
+        elif(choice == 3):
+
+            if(Memo.WorstFit(list_proc[n_wait],atual_clock)):
+                t_wait += atual_clock-list_proc[n_wait].get_arr()
+                n_wait += 1#Logica de t wait
+            else:
+                control = False
                 
     for i in range(len(list_proc)):
         if atual_clock == list_proc[i].get_end():
             Memo.out_proce(list_proc[i])
-            #list_proc[i].pop()
     
     print("-------------")
     print("clock: "+str(atual_clock))
     Memo.printf()
-    input()
-    print("------")
+    #l=input("Test")
+    print("-------------")
     print("\n")
     atual_clock += 1
-    Memo.graph(atual_clock)
-print("Numero de falhas "+ str(t_fail))
+    #Memo.graph(atual_clock)
+print("Numero de falhas "+ str(t_wait))
 a = t_wait/n_wait
 t_medio = time.time()-t_medio
 print("Numero medio de espera "+str(a))
